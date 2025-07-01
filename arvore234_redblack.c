021b4d50c58043c7d6a53d3b291b45e8b63ae40e
André <meloluizandre10@gmail.com>
2025-07-01 20:58:38 -0300

    Adição de comentários no código

--- arvore234_redblack.c
+++ arvore234_redblack.c
@@ -12,32 +12,39 @@ struct no{
     int folha;
 };
 
-
-
 arvoreB *alocaArvoreB(){
+    // Aloca uma árvore B
     arvoreB *novaArvore = (arvoreB*) malloc(sizeof(arvoreB));
     if(!novaArvore){
         return NULL;
     }
 
+    // Aloca um nó para a raiz
     noB *noRaiz = alocaNoB();
 
+    // Define a raiz como folha
     noRaiz->folha = 1;
 
+    // Define a ordem da árvore como 4
     novaArvore->ordem = M;
+
+    //Conecta a raiz criada à árvore
     novaArvore->raiz = noRaiz;
 
     return novaArvore;
 }
 
 noB *alocaNoB(){
+    // Aloca um novo nó
     noB *novoNo = (noB*) malloc(sizeof(noB));
     if(!novoNo){
         return NULL;
     }
 
+    //Aloca o vetor de chaves do nó
     int *vetChaves = (int*) malloc((M-1) * sizeof(int));
     
+    //Faz as definições do novo nó
     novoNo->vetChaves = vetChaves;
     for(int i=0; i < M; i++){
         novoNo->vetFilhos[i] = NULL;
@@ -53,9 +60,13 @@ void percorreArvoreB(noB *no){
     if(no != NULL){
         int i;
         for (i = 0; i < no->ocupacao; i++){
+            //Percorre a sub-árvore de cada um dos filhos
             percorreArvoreB(no->vetFilhos[i]);
+
+            //Imprime o valor da chave no vetor
             printf("%d ", no->vetChaves[i]);
         }
+        //Percorre a última sub-árvore do nó
         percorreArvoreB(no->vetFilhos[i]);
     }
 }
@@ -63,21 +74,24 @@ void percorreArvoreB(noB *no){
 void percorreArvoreBPreOrdem(noB *no){
     if(no != NULL){
         printf("[");
+        //Imprime cada um dos valores do nó
         for (int i = 0; i < no->ocupacao; i++){
             printf("%d ", no->vetChaves[i]);
         }
         printf("] ");
+        //Imprime cada uma das sub-árvores
         for(int i = 0; i <= no->ocupacao; i++){
             percorreArvoreBPreOrdem(no->vetFilhos[i]);
         }
     }
 }
 
-
 void insereChaveB(int chave, arvoreB *arv){
     noB *aux = arv->raiz;
 
     if(aux->ocupacao == M - 1){
+        //Faz o split da raiz caso ela esteja cheia
+        //É criada uma nova raiz para isso
         noB *novaRaiz = alocaNoB();
         novaRaiz->folha = 0;
         novaRaiz->vetFilhos[0] = aux;
@@ -85,9 +99,9 @@ void insereChaveB(int chave, arvoreB *arv){
         arv->raiz = novaRaiz;
         aux->noPai = novaRaiz;
     }
+    //Insere o valor caso a raiz não esteja cheia
     insereNaoCheio(arv->raiz, chave);
 }
-
 
 void insereNaoCheio(noB *no, int chave){
     int i = no->ocupacao - 1;
@@ -125,22 +139,25 @@ void insereNaoCheio(noB *no, int chave){
         //Insere o filho no próximo nó recursivamente
         //até encontrar o nó correto
         insereNaoCheio(no->vetFilhos[i], chave);
-
     }
 }
 
 void split(noB *pai, int posicaoFilho){
+    //Definição do grau mínimo (t) e do nó cheio
     int t = M/2;
     noB *noCheio = pai->vetFilhos[posicaoFilho];
 
+    //Cria um novo nó para o split
     noB *novoNo = alocaNoB();
     novoNo->folha = noCheio->folha;
     novoNo->ocupacao = t-1;
 
+    //Passa a segunda metade do vetor para o novo nó
     for(int i = 0; i < t-1; i++){
         novoNo->vetChaves[i] = noCheio->vetChaves[i+t];
     }
 
+    //Passa os filhos do nó para o novo nó, caso não seja folha
     if(noCheio->folha == 0){
         for(int i = 0; i < t; i++){
             novoNo->vetFilhos[i] = noCheio->vetFilhos[i + t];
@@ -148,20 +165,26 @@ void split(noB *pai, int posicaoFilho){
         }
     }
 
+    //Define a ocupação do nó cheio após o split
     noCheio->ocupacao = t-1;
 
+    //Encontra a posição do novo nó no nó pai
     for(int i = pai->ocupacao; i > posicaoFilho; i--){
         pai->vetFilhos[i+1] = pai->vetFilhos[i];
     }
 
+    //Faz a conexão do pai com o novo nó
     pai->vetFilhos[posicaoFilho+1] = novoNo;
 
+    //Encontra a posição da nova chave no nó pai
     for(int i = pai->ocupacao; i >= posicaoFilho; i--){
         pai->vetChaves[i+1] = pai->vetChaves[i];
     }
 
+    //Passa o valor mediano do nó cheio para o pai
     pai->vetChaves[posicaoFilho] = noCheio->vetChaves[t - 1];
 
+    //Ajusta a ocupação do pai
     pai->ocupacao++;
     printf("split!\n");
 
@@ -176,6 +199,7 @@ arvoreRB *converter234ParaRB(arvoreB *arv){
         novaRaiz->cor = 'P';
     }
 
+    //Aloca uma nova árvore e adiciona a raiz a ela
     arvoreRB *novaArvoreRB = alocaArvoreRB();
     novaArvoreRB->sentinela->fDir = novaRaiz;
 
@@ -185,6 +209,7 @@ arvoreRB *converter234ParaRB(arvoreB *arv){
 noRB *converterNo234(noB *noOriginal){
     if(noOriginal == NULL) return NULL;
 
+    //Declaração de variáveis
     noRB *noPai;
     noRB *noEsq;
     noRB *noDir;
@@ -193,8 +218,11 @@ noRB *converterNo234(noB *noOriginal){
         case 1://Nó com 1 de ocupação e 2 filhos
             //cria um nó preto único
             noPai = alocaNoRB(noOriginal->vetChaves[0], 'P');
+
+            //Chama a função recursivamente para os filhos
             noPai->fEsq = converterNo234(noOriginal->vetFilhos[0]);
             noPai->fDir = converterNo234(noOriginal->vetFilhos[1]);
+            
             return noPai;
             break;
         case 2://Nó com 2 de ocupação e 3 filhos
@@ -202,6 +230,8 @@ noRB *converterNo234(noB *noOriginal){
             noPai = alocaNoRB(noOriginal->vetChaves[1], 'P');
             noEsq = alocaNoRB(noOriginal->vetChaves[0], 'V');
 
+            //Chama a função recursivamente para os filhos
+            //e faz a conexão com o filho à esquerda
             noEsq->fEsq = converterNo234(noOriginal->vetFilhos[0]);
             noEsq->fDir = converterNo234(noOriginal->vetFilhos[1]);
             noPai->fEsq = noEsq;
@@ -215,14 +245,15 @@ noRB *converterNo234(noB *noOriginal){
             noEsq = alocaNoRB(noOriginal->vetChaves[0], 'V');
             noDir = alocaNoRB(noOriginal->vetChaves[2], 'V');
 
+            //Chama a função recursivamente para os filhos
             noEsq->fEsq = converterNo234(noOriginal->vetFilhos[0]);
             noEsq->fDir = converterNo234(noOriginal->vetFilhos[1]);
             noDir->fEsq = converterNo234(noOriginal->vetFilhos[2]);
             noDir->fDir = converterNo234(noOriginal->vetFilhos[3]);
 
+            //Faz as conexões com os filhos da esquerda e da direita
             noPai->fEsq = noEsq;
             noPai->fDir = noDir;
-            printf("cara. (1)");
 
             return noPai;
             break;
@@ -230,8 +261,10 @@ noRB *converterNo234(noB *noOriginal){
 }
 
 arvoreRB *alocaArvoreRB(){
+    //Aloca nova árvore RB
     arvoreRB* novaArvore = (arvoreRB*) malloc(sizeof(arvoreRB));
 
+    //Define os valores iniciais da árvore
     noRB* sentinela = alocaNoRB(-1000, 'P');
     novaArvore->sentinela = sentinela;
     novaArvore->alturaPreto = 0;
@@ -240,8 +273,10 @@ arvoreRB *alocaArvoreRB(){
 }
 
 noRB *alocaNoRB(int chave, char cor){
+    //Aloca novo nó RB
     noRB* novoNo= (noRB*) malloc(sizeof(noRB));
 
+    //Define os valores iniciais do nó
     novoNo->chave=chave;
     novoNo->cor=cor;
     novoNo->fEsq=NULL;
@@ -255,7 +290,10 @@ noRB *alocaNoRB(int chave, char cor){
 void percorreArvoreRBPreOrdem(arvoreRB *arv, noRB *aux){
     if(!aux)
         return;
+    //Imprime a raiz
     printf("%d -- %c -- %d\n", aux->chave, aux->cor, aux->alturaPreto);
+    //Percorre sub-árvore da esquerda
     percorreArvoreRBPreOrdem(arv, aux->fEsq);
+    //Percorre sub-árvore da direita
     percorreArvoreRBPreOrdem(arv, aux->fDir);
 }
