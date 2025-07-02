8b8a1dfd27a709e98da9d1f620cc8752579b49db
André <meloluizandre10@gmail.com>
2025-07-01 21:04:42 -0300

    Adição de comentários no arquivo .h

--- arvore234_redblack.h
+++ arvore234_redblack.h
@@ -29,25 +29,28 @@ struct noRB{
 };
 
 
-// Operações árvore B
+// ------------------------- Operações árvore B ------------------------- //
 
-//Aloca uma nova árvore que recebe a ordem da árvore
+//Aloca uma nova árvore B
 arvoreB *alocaArvoreB();
 
-//Aloca um novo nó
-//Recebe a ordem
+//Aloca um novo nó da árvore B
 noB *alocaNoB();
 
+//Percorre a árvore B em ordem a partir de um nó
 void percorreArvoreB(noB *no);
 
+//Percorre a árvore B em pré-ordem a partir de um nó
 void percorreArvoreBPreOrdem(noB *no);
 
 //Insere uma nova chave na árvore
 void insereChaveB(int chave, arvoreB *arv);
 
+//Insere uma nova chave em um nó não cheio
 void insereNaoCheio(noB *no, int chave);
 
 //Faz o split de um nó cheio
+//Recebe o pai do nó cheio e a posição do nó cheio no vetor de filhos
 void split(noB *pai, int posicaoFilho);
 
 //Remove uma chave da árvore
@@ -60,17 +63,28 @@ void rotacao(noB *noDesbalanceado, noB *noIrmao);
 //Faz o merge entre o nó desbalanceado e um irmão
 void merge(noB *noDesbalanceado, noB *noIrmao);
 
+// ------------------------- Operações de conversão ------------------------- //
+
+//Converte uma árvore 234 em RB
+//Recebe uma árvore 234
+//Retorna uma árvore RB
 arvoreRB *converter234ParaRB(arvoreB *arv);
 
+//Converte um nó da árvore 234 em um nó da árvore RB
+//Recebe um nó da árvore 234
+//Retorna o nó pai gerado
 noRB *converterNo234(noB *no);
 
-// Operações Árvore RB
+// ------------------------- Operações árvore RB ------------------------- //
 
-
+//Aloca uma árvore RB
 arvoreRB *alocaArvoreRB();
 
+//Aloca um nó da árvore RB
+//Recebe a chave e a cor do nó
 noRB *alocaNoRB(int chave, char cor);
 
+//Percorre a árvore RB em pré-ordem a partir de um nó
 void percorreArvoreRBPreOrdem(arvoreRB *arv, noRB *aux);
 
 #endif
